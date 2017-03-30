31b86ba74aa93150685d75b7c1844abaf2d92834
Sascha Adler <sascha.adler@canadiana.ca>
2017-03-30 15:18:26 -0400

    Adding koa-route for basic routing

--- src/server.js
+++ src/server.js
@@ -3,13 +3,15 @@ const copresentationEndpoint = env.requireUrl('COPRESENTATION_ENDPOINT');
 const iiifiEndpoint = env.requireUrl('IIIFI_ENDPOINT');
 
 const Koa = require('koa');
+const router = require('koa-route');
+
 const app = new Koa();
 
-app.use(ctx => {
+app.use(router.get('/', ctx => {
   ctx.body = {
     version: '2.1',
     documentation: 'http://iiif.io/api/presentation/2.1/'
   };
-});
+}));
 
 app.listen(3000);
