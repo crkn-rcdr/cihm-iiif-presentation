bc9981972e2b2bea9bef7e40ef98e164dd44a332
Sascha Adler <sascha.adler@canadiana.ca>
2017-04-10 18:02:27 -0400

    Adds couch request for fetching components for an item

--- src/copresentation/request.js
+++ src/copresentation/request.js
@@ -10,15 +10,46 @@ const copresentation = rp.defaults({
   method: 'GET'
 });
 
-module.exports = async function request(path) {
-  let options = { uri: path };
+module.exports.single = async (key) => {
+  let options = { uri: key };
 
   let v, responseBody;
   try {
     responseBody = await copresentation(options);
     v = { statusCode: 200, data: responseBody };
   } catch(e) {
-    v = { statusCode: e.statusCode, error: e.error };
+    v = { statusCode: e.statusCode || 500, error: e.error || {} };
   }
+  return v;
+};
+
+module.exports.components = async (key) => {
+  let options = {
+    uri: '_all_docs',
+    qs: {
+      startkey: `"${key}."`,
+      endkey: `"${key}.\u00FF"`,
+      include_docs: true
+    }
+  };
+
+  let v, responseBody;
+  try {
+    responseBody = await copresentation(options);
+    if (responseBody.rows.length) {
+      v = {
+        statusCode: 200,
+        data: responseBody.rows.reduce((obj, current) => {
+          obj[current.key] = current.doc;
+          return obj;
+        }, {})
+      };
+    } else {
+      v = { statusCode: 404, error: { error: 'not_found', reason: 'no rows' } };
+    }
+  } catch(e) {
+    v = { statusCode: e.statusCode || 500, error: e.error || {} };
+  }
+
   return v;
 };
